3e65c150c74134ac35f74b450589f3f4001dbacd
based <based@arch>
2025-05-10 17:10:09 -0400

    remove debug code

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 #include "string.h"
 #include "syntax.h"
 #include <stdio.h>
-#include <string.h>
 
 static const int part_sz = kilobyte / 4;
 static string read_source(arena *a, int argc, char **argv)
@@ -32,68 +31,6 @@ static int write_output(string output, int argc, char **argv)
         // eventually, optionally write to other places
         fprintf(stdout, "%.*s", output.len, output.addr);
         return 0;
-}
-
-static void debug_tokens(token_string tokens)
-{
-        token *tok;
-        string *tok_val;
-        for (int i = 0; i < tokens.len; i++) {
-                tok = tokens.arr + i;
-                switch (tok->type) {
-                default:
-                        printf("%i: %s\n", tok->line_number,
-                               token_names[tok->type]);
-                        break;
-                case TOK_LITERAL:
-                        printf("%i: %s - %li\n", tok->line_number,
-                               token_names[tok->type], tok->i_value);
-                        break;
-                case TOK_REGISTER:
-                        printf("%i: %s - %li\n", tok->line_number,
-                               token_names[tok->type], tok->i_value);
-                        break;
-                case TOK_LABEL:
-                        tok_val = &tok->s_value;
-                        char mem[tok_val->len + 1];
-                        memcpy(mem, tok_val->addr, tok_val->len);
-                        mem[tok_val->len] = '\0';
-                        printf("%i: %s - %s\n", tok->line_number,
-                               token_names[tok->type], mem);
-                        break;
-                }
-        }
-}
-
-static void debug_instruction(instr ins)
-{
-        switch (ins.type) {
-        case I_MOV:
-                printf("\tmov\n");
-                break;
-        case I_ADD:
-                printf("\tadd\n");
-                break;
-        case I_JEQ:
-                printf("\tjeq\n");
-                break;
-        case I_JMP:
-                printf("\tjmp\n");
-                break;
-        }
-}
-
-static void debug_program(program prog)
-{
-        instr_seq seq;
-        for (int i = 0; i < prog.len; i++) {
-                seq = prog.blocks[i];
-                string label = seq.label.s_value;
-                printf("%.*s %i:\n", label.len, label.addr, seq.len);
-                for (int j = 0; j < seq.len; j++) {
-                        debug_instruction(seq.arr[j]);
-                }
-        }
 }
 
 int main(int argc, char **argv)
